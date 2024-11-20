83713b9f4ce24827c84dc9a2012c6a3f7bd65d58
Auchendale <kpnparagon@sky.com>
2024-11-20 15:47:58 +0000

    add a letter posting endpoint

--- controllers/letters.controllers.js
+++ controllers/letters.controllers.js
@@ -1,5 +1,6 @@
 const { mongoose } = require("mongoose");
-const Letters = require("../models/letters.js");
+const Letter = require("../models/letters.js");
+const User = require("../models/users.js")
 const ObjectId = require("mongoose").Types.ObjectId;
 
 const getLetterByLetterID = async (request, response, next) => {
@@ -8,11 +9,32 @@ const getLetterByLetterID = async (request, response, next) => {
     response.status(400).send({ message: "bad request" });
   } else {
     const id = new ObjectId(letter_id);
-    const letter = await Letters.findById(id);
+    const letter = await Letter.findById(id);
     if (!letter)
       response.status(404).send({ message: "letter does not exist" });
     else response.status(200).send({ letter });
   }
 };
 
-module.exports = { getLetterByLetterID };
+const postLetter = async (request, response, next) => {
+  const { sender, recipient, content } = request.body
+  if(!sender || !recipient || !content){
+    response.status(400).send({message: "bad request"})
+  }
+  try {
+    const senderExists = await User.findOne({ username: sender })
+    const recipientExists = await User.findOne({ username: recipient })
+
+    if(!senderExists || !recipientExists){
+      response.status(404).send({message: "sender or recipient are not users"})
+    }
+    const letter = new Letter({ sender, recipient, content})
+    const savedLetter = await letter.save()
+    response.status(201).send({ letter })
+  }
+  catch (error) {
+    next(error)
+  }
+}
+
+module.exports = { getLetterByLetterID, postLetter };
