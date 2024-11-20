cd306b5cd96502cb95dc3562c82ba186891ccfae
Auchendale <kpnparagon@sky.com>
2024-11-20 16:18:44 +0000

    adds an endpoint which returns all letters recieved by a user

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,15 +1,14 @@
 const User = require("../models/users.js");
 
-const getUsers = async (request, response, next) => {
+exports.getUsers = async (request, response, next) => {
   const users = await User.find();
   response.status(200).send({ users });
 };
 
-const getUserByUsername = async (request, response, next) => {
+exports.getUserByUsername = async (request, response, next) => {
   const { username } = request.params;
   const user = await User.findOne({ username });
-  if (!user) response.status(404).send({ msg: "user does not exist" });
+  if (!user) response.status(404).send({ message: "user does not exist" });
   else response.status(200).send({ user });
 };
 
-module.exports = { getUsers, getUserByUsername };
